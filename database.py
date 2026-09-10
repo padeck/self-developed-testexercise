0c054452149e09143e8aeacd8c0a98b2ba339715
padeck <patrick.a.deck@gmail.com>
2026-09-10 12:29:18 +0200

    changed db url to be loaded from the .env file

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = "sqlite:///./tickets.db"
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(
     DATABASE_URL,
