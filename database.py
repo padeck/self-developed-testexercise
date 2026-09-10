1cf44915f2111724f001f6e1d8ea0271d804671c
padeck <patrick.a.deck@gmail.com>
2026-09-10 12:36:04 +0200

    load neon postgres connection string from .env and use this for the db

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
+load_dotenv()
 DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     bind=engine,
